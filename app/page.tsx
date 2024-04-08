72d98894a940455ea259342ae709b4714f872aad
vivek-a666 <vivek.mails09@gmail.com>
2024-04-08 19:02:05 +0530

    fix: Dynamic error usage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 "use client";
+
 import Feed from "@/Components/Feed";
 import Sidebar from "@/Components/Sidebar";
 import Widgets from "@/Components/Widgets";
-import '../app/globals.css'
+import '../app/globals.css';
 import { fetchPosts } from "@/utils/fetchPosts";
 import { Post } from "@/typings";
-import {Toaster} from 'react-hot-toast'
+import { Toaster } from 'react-hot-toast';
 
 async function getData(): Promise<Post[]> {
   const posts = await fetchPosts();
@@ -14,17 +15,14 @@ async function getData(): Promise<Post[]> {
 
 export default async function Home() {
   const posts = await getData();
-
   return (
     <div className="mx-auto lg:max-w-6xl max-h-screen overflow-y-auto">
       <Toaster />
       <main className="grid grid-cols-9">
         <Sidebar />
-          <Feed posts={posts}/>
-        
+        <Feed posts={posts} />
         <Widgets />
       </main>
-
     </div>
   );
 }
--- utils/fetchPosts.ts
+++ utils/fetchPosts.ts
@@ -1,10 +1,8 @@
 import { Post } from "@/typings";
 
-export const fetchPosts = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/posts`)
-    const data = await res.json();
-    const posts:Post[] = data.posts;
-
-    return posts
-
-}
+export const fetchPosts = async (): Promise<Post[]> => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/posts`);
+  const data = await res.json();
+  const posts: Post[] = data.posts;
+  return posts;
+};
