643744048975c0f989cfe50fa75895e92e168a46
vivek-a666 <vivek.mails09@gmail.com>
2024-04-08 18:42:53 +0530

    fix Icon wrapper

--- Components/Sidebar.tsx
+++ Components/Sidebar.tsx
@@ -21,14 +21,14 @@ function Sidebar() {
   return (
     <div className='col-span-2 flex flex-col items-center px-4 md:items-start'>
         <Image className='h-10 w-10' src= {logo} alt="" />
-        <SidebarRow Icon={HomeIcon} title="Home" />
-        <SidebarRow Icon={HashtagIcon} title="Explore" />
-        <SidebarRow Icon={BellIcon} title="Notifications" />
-        <SidebarRow Icon={EnvelopeIcon} title="Messages" />
-        <SidebarRow Icon={BookmarkIcon} title="Bookmarks" />
-        <SidebarRow Icon={CircleStackIcon} title="Lists" />
-        <SidebarRow Icon={EllipsisHorizontalCircleIcon} title="More" />
-        <SidebarRow onClick={session? signOut:signIn} Icon={UserIcon} title= {session? 'Sign Out':'Sign In'} />
+        <SidebarRow Icon={HomeIcon as (props: React.SVGProps<SVGSVGElement>) => JSX.Element} title="Home" />
+        <SidebarRow Icon={HashtagIcon as (props: React.SVGProps<SVGSVGElement>) => JSX.Element} title="Explore" />
+        <SidebarRow Icon={BellIcon as (props: React.SVGProps<SVGSVGElement>) => JSX.Element} title="Notifications" />
+        <SidebarRow Icon={EnvelopeIcon as (props: React.SVGProps<SVGSVGElement>) => JSX.Element} title="Messages" />
+        <SidebarRow Icon={BookmarkIcon as (props: React.SVGProps<SVGSVGElement>) => JSX.Element} title="Bookmarks" />
+        <SidebarRow Icon={CircleStackIcon as (props: React.SVGProps<SVGSVGElement>) => JSX.Element} title="Lists" />
+        <SidebarRow Icon={EllipsisHorizontalCircleIcon as (props: React.SVGProps<SVGSVGElement>) => JSX.Element} title="More" />
+        <SidebarRow onClick={session? signOut:signIn} Icon={UserIcon as (props: React.SVGProps<SVGSVGElement>) => JSX.Element} title= {session? 'Sign Out':'Sign In'} />
 
 
     </div>
